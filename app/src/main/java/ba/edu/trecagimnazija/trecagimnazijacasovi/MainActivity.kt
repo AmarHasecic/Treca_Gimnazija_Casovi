990e836e593e4ee2194597caa2bbbf28355eb686
AmarHasecic <ahasecic1@etf.unsa.ba>
2023-11-14 23:44:55 +0100

    Programski kod naše prve android aplikacije

--- app/src/main/java/ba/edu/trecagimnazija/trecagimnazijacasovi/MainActivity.kt
+++ app/src/main/java/ba/edu/trecagimnazija/trecagimnazijacasovi/MainActivity.kt
@@ -3,13 +3,19 @@ package ba.edu.trecagimnazija.trecagimnazijacasovi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ba.edu.trecagimnazija.trecagimnazijacasovi.ui.theme.TrecaGimnazijaCasoviTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Pozdrav(tekstPozdrava = "Hello Android")
                 }
             }
         }
@@ -30,17 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Pozdrav(tekstPozdrava: String){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrecaGimnazijaCasoviTheme {
-        Greeting("Android")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        LazyColumn() {
+            item {
+                Text(
+                    text = tekstPozdrava,
+                    fontSize = 50.sp,
+                    color = Color.Cyan
+                )
+            }
+
+            // Možemo dodati koliko god želimo itema u kolonu
+
+        }
     }
 }
+
+
