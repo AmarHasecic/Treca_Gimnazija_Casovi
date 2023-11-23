e5e712d14befd3cfdfcbd9cbba6caa25f3f236a0
Amar Hasečić <amarh@gimnazija.local>
2023-11-23 16:57:38 +0100

    Implementacij brojaca

--- app/src/main/java/ba/edu/trecagimnazija/trecagimnazijacasovi/MainActivity.kt
+++ app/src/main/java/ba/edu/trecagimnazija/trecagimnazijacasovi/MainActivity.kt
@@ -3,13 +3,26 @@ package ba.edu.trecagimnazija.trecagimnazijacasovi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ba.edu.trecagimnazija.trecagimnazijacasovi.ui.theme.TrecaGimnazijaCasoviTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +35,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BrojacScreen()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BrojacScreen(){
+
+    var brojac: Int by remember { mutableStateOf(0) }
+    //var brojac by remember { mutableStateOf(0) }
+
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Center
+    ) {
+    Column(
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = brojac.toString(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 50.sp
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(onClick = { brojac++ }) {
+            Text(text = "Povecaj broj za 1")
+        }
+        Button(onClick = { brojac-- }) {
+            Text(text = "Smanji broj za 1")
+        }
+        Button(onClick = { brojac = 0 }) {
+            Text(text = "Vrati broj na 0")
+        }
+    }
+ }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrecaGimnazijaCasoviTheme {
-        Greeting("Android")
-    }
-}
