9df477375aa9399978ff8394b8490c0e7c3d3b44
AmarHasecic <ahasecic1@etf.unsa.ba>
2024-02-12 11:44:28 +0100

    Text Fields

--- app/src/main/java/ba/edu/trecagimnazija/trecagimnazijacasovi/MainActivity.kt
+++ app/src/main/java/ba/edu/trecagimnazija/trecagimnazijacasovi/MainActivity.kt
@@ -3,13 +3,26 @@ package ba.edu.trecagimnazija.trecagimnazijacasovi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ba.edu.trecagimnazija.trecagimnazijacasovi.ui.theme.TrecaGimnazijaCasoviTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +35,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                     MainFunction()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainFunction(){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrecaGimnazijaCasoviTheme {
-        Greeting("Android")
+    var text by remember { mutableStateOf("Hello") }
+
+    Box(
+      modifier = Modifier.fillMaxSize(),
+      contentAlignment = Alignment.Center
+    ){
+
+        Column{
+
+           TextField(
+               value = text,
+               onValueChange = { text = it },
+               label = { Text("Zdravo") }
+               )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text("Zdravo") }
+            )
+
+        }
     }
 }
